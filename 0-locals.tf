a49357ba7528b68dcc1bc18b49b685b8f4248210
Jiean Yang <jiean.yang.externe@emeria.eu>
2024-11-15 22:10:17 +0100

    Update codes

--- 0-locals.tf
+++ 0-locals.tf
@@ -1,5 +1,5 @@
 locals {
-  env         = "staging"
+  env         = "staging-xiaoka"
   region      = "us-east-2"
   zone1       = "us-east-2a"
   zone2       = "us-east-2b"
--- 2-vpc.tf
+++ 2-vpc.tf
@@ -5,6 +5,6 @@ resource "aws_vpc" "main" {
   enable_dns_hostnames = true
 
   tags = {
-    Name = "${locals.env}-main"
+    Name = "${local.env}-main"
   }
 }
