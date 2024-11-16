b02065e9e59fdcc5197543e9c346c8b904c82e1a
Jiean Yang <jiean.yang.externe@emeria.eu>
2024-11-16 21:10:33 +0100

    Update files structure

--- 12.1-metric-server.bash
+++ 12.1-metric-server.bash
@@ -4,3 +4,6 @@ helm repo update
 kubectl get pods -n kube-system
 
 kubectl logs -l app.kubernetes.io/instance=metrics-server -n kube-system
+
+kubectl top pods -n kube-system
+kubectl top nodes -n kube-system
