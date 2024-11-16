977a96461696128ad4fc32e91ccbd04cd5dab043
Jiean Yang <jiean.yang.externe@emeria.eu>
2024-11-16 12:45:51 +0100

    Create 9-add-developer-user.tf

---
+++ 9-add-developer-user.tf
@@ -0,0 +1,35 @@
+resource "aws_iam_user" "developer" {
+  name = "developer"
+}
+
+# Grant the developer user the ability to describe and list EKS clusters to be able to update Kubernetes config and connect to the cluster
+resource "aws_iam_policy" "developer_eks" {
+  name = "AmazonEKSDeveloperPolicy"
+
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "eks:DescribeCluster",
+                "eks:ListClusters"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+POLICY
+}
+
+resource "aws_iam_user_policy_attachment" "developer_eks" {
+  user       = aws_iam_user.developer.name
+  policy_arn = aws_iam_policy.developer_eks.arn
+}
+
+resource "aws_eks_access_entry" "developer" {
+  cluster_name      = aws_eks_cluster.eks.name
+  principal_arn     = aws_iam_user.developer.arn
+  kubernetes_groups = ["my-viewer"]
+}
---
+++ 9.1-add-developer-user.bash
@@ -0,0 +1,14 @@
+# Update .kube/config credentials with the new EKS cluster
+aws eks update-kubeconfig --region us-east-2 --name staging-demo --profile developer
+
+kubectl config view --minify
+
+kubectl get pods
+
+kubectl auth can-i get pods # yes
+kubectl auth can-i create pods # no
+
+kubectl get nodes
+kubectl auth can-i get nodes # yes
+
+kubectl auth can-i '*' '*' # no
