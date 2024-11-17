33a6a97744ff9043d8bc2d1ff8a34f7c1573419e
Jiean Yang <jiean.yang.externe@emeria.eu>
2024-11-17 11:59:19 +0100

    Create 14-cluster-autoscaler.tf

---
+++ 14-cluster-autoscaler.tf
@@ -0,0 +1,69 @@
+resource "aws_iam_role" "cluster_autoscaler" {
+  name = "${aws_eks_cluster.eks.name}-cluster-autoscaler"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "sts:AssumeRole",
+          "sts:TagSession"
+        ],
+        Principal = {
+          Service = "pods.eks.amazonaws.com", # Trust relationship: this is the service account used by the cluster autoscaler
+        },
+      },
+    ],
+  })
+}
+
+resource "aws_iam_policy" "cluster_autoscaler" {
+  name = "${aws_eks_cluster.eks.name}-cluster-autoscaler"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Statement = [
+          {
+            Effect = "Allow",
+            Action = [
+              "autoscaling:DescrieAutoScalingGroups",
+              "autoscaling:DescribeAutoScalingInstances",
+              "autoscaling:DescribeLaunchConfiguraitons",
+              "autoscaling:DescribeScalingActivities",
+              "autoscaling:DescribeTags",
+              "ec2:DescribeImages",
+              "ec2:DescribeInstanceTypes",
+              "ec2:DescribeLaunchTemplateVersions",
+              "ec2:GetInstanceTypesFromInstanceRequirements",
+              "eks:DescribeNodegroup",
+            ]
+          }
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "autoscaling:SetDesiredCapacity",
+          "autoscaling:TernimateInstanceInAutoScalingGroup"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "cluster_autoscaler" {
+  policy_arn = aws_iam_policy.cluster_autoscaler.arn
+  role       = aws_iam_role.cluster_autoscaler.name
+}
+
+resource "aws_eks_pod_identity_association" "cluster_autoscaler" {
+  cluster_name    = aws_eks_cluster.eks.name
+  namespace       = "kube-system"
+  service_account = "cluster-autoscaler"
+  role_arn        = aws_iam_role.cluster_autoscaler.arn
+}
