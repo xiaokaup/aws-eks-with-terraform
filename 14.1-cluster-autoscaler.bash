43f5429733ddfb13d053feefd016ba5eb7ccfe9c
Jiean Yang <jiean.yang.externe@emeria.eu>
2024-11-17 12:40:55 +0100

    Create eks yml files about cluster-autoscaler

--- 14.1-cluster-autoscaler.bash
+++ 14.1-cluster-autoscaler.bash
@@ -3,3 +3,11 @@ terragrunt apply
 kubectl get pods -n kube-system
 
 kubectl logs -l app.kubernetes.io/instance=autoscaler -f -n kube-system
+
+
+watch -t kubectl get nodes -n 4-example
+watch -t kubectl get pods -n 4-example
+
+kubectl apply -f eks/3-cluster-autoscaler
+
+kubectl delete ns 4-example
