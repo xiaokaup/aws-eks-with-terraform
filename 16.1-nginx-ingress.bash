8dd62059f2ead898e30c13070ca64e0f72daab8b
Jiean Yang <jiean.yang.externe@emeria.eu>
2024-11-17 22:35:37 +0100

    Create 16.1-nginx-ingress.bash and namesapce 8-example case

--- 16.1-nginx-ingress.bash
+++ 16.1-nginx-ingress.bash
@@ -1,3 +1,4 @@
+# Infrastructure as Code (IaC) with Terraform
 terragrunt apply
 
 kubectl get svc -n ingress
@@ -5,3 +6,13 @@ kubectl get svc -n ingress
 kubectl get pods -o wide -n ingress
 
 kubectl get ingressclass
+
+
+# Apply nginx ingress setup in kubernetes cluster
+kubectl apply -f eks/7-nginx-ingress
+
+kubectl get ing -n 8-example
+
+curl -i --header "Host: ex8.antonputra.com" http://k8s-ingress-external-6695833531-da85e3e9f56d4bec.elb.us-east-2.amazonaws.com/about
+
+kubectl delete ns 8-example
