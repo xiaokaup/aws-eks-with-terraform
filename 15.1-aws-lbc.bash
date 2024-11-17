429267d3604ebc869a2959436d7a125137aad8e3
Jiean Yang <jiean.yang.externe@emeria.eu>
2024-11-17 17:16:23 +0100

    Second example for 15-aws-lbc.tf

--- 15.1-aws-lbc.bash
+++ 15.1-aws-lbc.bash
@@ -2,8 +2,24 @@ terragrunt apply
 
 kubectl get pods -n kube-system
 
+# First example, create load balancer service with pod IPs
 kubectl apply -f eks/4-lbc
 
 kubectl get svc -n 5-example
 
+kubectl get pods -o wide -n 5-example
+
+curl -i http://k8s-5example-myapp-d5bf4493a2-2e9c6d78dd44e10f.elb.us-east-2.amazonaws.com:8080/about
+
 kubectl delete ns 5-example
+
+# Second example
+kubectl get ingressclass
+
+kubectl apply -f eks/5-lbc-second-example
+
+kubectl get ing -n 6-example
+
+curl -i --header "Host: ex6.antonputra.com" http://k8s-6example-myapp-c79dafe9b7-1638754451.us-east-2.elb.amazonaws.com/about
+
+kubectl delete ns 6-example
