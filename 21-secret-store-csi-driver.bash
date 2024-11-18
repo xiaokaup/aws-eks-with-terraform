88247b71a2fd76dc696b526f874720de20e8bb26
Jiean Yang <jiean.yang.externe@emeria.eu>
2024-11-18 22:18:21 +0100

    Create 12-example

--- 21-secret-store-csi-driver.bash
+++ 21-secret-store-csi-driver.bash
@@ -1,2 +1,15 @@
 terragrunt apply
 
+kubectl logs -l app.kubernetes.io/component=csi-driver -n kube-system -f
+
+kubectl logs -l app.kubernetes.io/instance=secrets-store-csi-driver -n kube-system -f
+
+
+kubectl apply -f eks/11-secret-store-csi-driver
+
+kubectl get pods -n 12-example
+
+kubectl exec -it -n 12-example <pod_id> -- bash
+kubectl exec -it -n 12-example myapp-7586b85655-77d7v -- bash
+
+kubectl delete ns 12-example
