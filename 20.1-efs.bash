44937296dd0eb6c9e6aaa51d0ccfff58a4fef154
Jiean Yang <jiean.yang.externe@emeria.eu>
2024-11-18 18:33:24 +0100

    Add eks/10-efs-csi-driver example

--- 20.1-efs.bash
+++ 20.1-efs.bash
@@ -2,3 +2,14 @@ terragrunt init
 terragrunt apply
 
 kubectl get pods -n kube-system
+
+kubectl logs -l app.kubernetes.io/instance=aws-efs-csi-driver -n kube-system -f
+
+
+kubectl apply -f eks/10-efs-csi-driver
+
+kubectl get pods -n 11-example
+
+kubectl get pvc -n 11-example
+
+kubectl delete ns 11-example
